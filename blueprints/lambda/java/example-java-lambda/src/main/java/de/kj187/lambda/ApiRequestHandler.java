07127a9d5159bc137cf5ab35ebd1ccc6d6398543
Julian Kleinhans <julian.kleinhans@aoe.com>
2017-01-10 08:55:34 +0100

    Add an Java lambda example

--- blueprints/lambda/java/example-java-lambda/src/main/java/de/kj187/lambda/ApiRequestHandler.java
+++ blueprints/lambda/java/example-java-lambda/src/main/java/de/kj187/lambda/ApiRequestHandler.java
@@ -4,11 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class ApiRequestHandler implements RequestHandler<String, String> {
-
-    public String handleRequest(String request, Context context) {
-        context.getLogger().log("Request is");
-        context.getLogger().log(request);
-        return "Your input as output: " + request;
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Request is: " + input);
+        context.getLogger().log("EnvironmentName is: " + System.getenv("EnvironmentName"));
+        return "Your input as output: " + input + "\nEnvVar: " + System.getenv("EnvironmentName");
     }
-
 }
