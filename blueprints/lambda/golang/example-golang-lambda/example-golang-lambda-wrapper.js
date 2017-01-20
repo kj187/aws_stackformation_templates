68ee248f92d4eae63c3d5337e2914b137bda6039
Julian Kleinhans <julian.kleinhans@aoe.com>
2017-01-20 14:13:15 +0100

    Add Golang lambda example

---
+++ blueprints/lambda/golang/example-golang-lambda/example-golang-lambda-wrapper.js
@@ -0,0 +1,9 @@
+
+var spawn = require('child_process').spawn;
+
+exports.handler = function(event, context) {
+    var child = spawn('./example-golang-lambda', [JSON.stringify(event, null, 2)]);
+    child.stdout.on('data', function (data) { console.log("stdout:\n"+data); });
+    child.stderr.on('data', function (data) { console.log("stderr:\n"+data); });
+    child.on('close', function (code) { context.done(); });
+}
---
+++ blueprints/lambda/golang/example-golang-lambda/example-golang-lambda.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+  	"os"
+)
+
+func main() {
+	fmt.Println("Hello Golang Lambda from log")
+	fmt.Printf("Your args: %v", os.Args)
+}
